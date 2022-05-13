829a9951b49b4e3124484a15ed51223d6c7dc274
adnankhateeb <adnankhateeb21@gmail.com>
2022-05-13 23:00:42 +0530

    imgur

--- imgur/scripts/index.js
+++ imgur/scripts/index.js
@@ -1,19 +1,41 @@
 import { makeAPICall, appendImages } from "../scripts/main.js";
+let page = 1;
 
-let res = await makeAPICall(
-   "https://api.unsplash.com/photos/?client_id=bESk1zhXjAJAd4AWL9VjlNKhx6Ijt23sJd1UlE5GdJc&per_page=30"
-);
+let res = await makeAPICall(page);
 
 let container = document.getElementById("container");
 appendImages(res, container);
 
 let search = document.querySelector(".search");
 
-search.addEventListener("keypress", (e) => {
-   let search_value = search.value;
+let debounceSearch;
 
-   if (e.key === "Enter") {
-      localStorage.setItem("search_term", search_value);
-      window.location.href = "./search.html";
+search.addEventListener("input", () => {
+   if (debounceSearch) {
+      clearTimeout(debounceSearch);
    }
+
+   debounceSearch = setTimeout(() => {
+      searchData();
+   }, 800);
 });
+
+function searchData() {
+   let query = search.value;
+   console.log("query:", query);
+
+   fetch(
+      `https://api.unsplash.com/search/photos?page=1&query=${query}&client_id=bESk1zhXjAJAd4AWL9VjlNKhx6Ijt23sJd1UlE5GdJc`
+   )
+      .then((res) => res.json())
+      .then((data) => {
+         console.log("data:", data.results);
+         appendImages(data.results, container);
+      });
+}
+
+let nextPage = document.getElementById("nextPage");
+nextPage.addEventListener("click", makeAPICall(page + 1));
+
+let prevPage = document.getElementById("prevPage");
+prevPage.addEventListener("click", makeAPICall(page - 1));
--- imgur/scripts/main.js
+++ imgur/scripts/main.js
@@ -1,6 +1,8 @@
-let makeAPICall = async (url) => {
+let makeAPICall = async (page) => {
    try {
-      let data = await fetch(url);
+      let data = await fetch(
+         `https://api.unsplash.com/photos/?client_id=bESk1zhXjAJAd4AWL9VjlNKhx6Ijt23sJd1UlE5GdJc&per_page=30&page=${page}`
+      );
 
       let res = await data.json();
       console.log("res:", res);
@@ -11,6 +13,7 @@ let makeAPICall = async (url) => {
 };
 
 let appendImages = (res, main) => {
+   main.innerHTML = "";
    res.forEach((el) => {
       //   console.log("el:", el)
       let image = document.createElement("img");
@@ -19,12 +22,11 @@ let appendImages = (res, main) => {
       let name = document.createElement("p");
       name.innerText = el.user.name;
 
-      let likes = document.createElement('p');
+      let likes = document.createElement("p");
       likes.innerText = "👍🏻 " + getRandomArbitrary(0, 1000);
 
-
       const comments = document.createElement("p");
-      comments.textContent = "💬 "+ getRandomArbitrary(0, 100);
+      comments.textContent = "💬 " + getRandomArbitrary(0, 100);
 
       const views = document.createElement("p");
       views.innerText = "👁️ " + getRandomArbitrary(101, 150) + "k";
