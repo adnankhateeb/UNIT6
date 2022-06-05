07a7e3d9a7dc817ac9e31126b6c9c5fa95069c2b
adnankhateeb <adnankhateeb21@gmail.com>
2022-06-05 21:44:40 +0530

    added backend

---
+++ webscrap/backend/app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+
+const mongoose = require("mongoose");
+const app = express();
+const jobListings = require("../AllDetails.json");
+
+const connect = () => {
+   return mongoose.connect(
+      "mongodb+srv://adnan:adnan@cluster0.rlrcf.mongodb.net/todo?retryWrites=true&w=majority"
+   );
+};
+
+app.listen(5000, async () => {
+   try {
+      await connect();
+      console.log("listening on port 5000");
+   } catch (error) {
+      console.log({ error });
+   }
+});
+
+app.get("/", async (req, res) => {
+   try {
+      return res.status(200).send(jobListings);
+   } catch (error) {
+      return res.status(400).send({ error });
+   }
+});
