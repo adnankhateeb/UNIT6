a3187e8143cc815ae2f3163581d1434302da84b1
adnankhateeb <adnankhateeb21@gmail.com>
2022-06-06 14:04:29 +0530

    backend done

---
+++ entity/client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import AllMovies from './Components/AllMovies';
+
+function App() {
+  return (
+    <div className="App">
+      <AllMovies />
+    </div>
+  );
+}
+
+export default App;
